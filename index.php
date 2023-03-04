4f258dddb4834004c1a996b18073fece18f7fefa
redentaculod12 <123133087+redentaculod12@users.noreply.github.com>
2023-03-04 09:00:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	    <meta charset ="UTF-8"> 
 		<meta name ="viewport" content ="width=device-width,initial-scale=1">
 		<title>CCTMHMS</title>
-		<link rel="stylesheet" href="CCTMHMS.github.io/assets/css/login.css">
+		<link rel="stylesheet" href="assets/css/login.css">
 	</head>
 	<body>
 		<div class='Container'>
